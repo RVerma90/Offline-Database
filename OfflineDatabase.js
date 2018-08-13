3f3b1bdc0c2971ffc3af43a11899da3d4eee30cf
RVerma90 <irahulverma90@gmail.com>
2018-08-13 11:49:12 +0100

    Project base

---
+++ OfflineDatabase.js
@@ -0,0 +1,10 @@
+// node-persist will be used for easy data persistence offline
+const storage = require('node-persist');
+const uuid = require('uuid/v4');
+
+module.exports = class OfflineDatabase {
+  constructor() {
+
+  }
+
+};
