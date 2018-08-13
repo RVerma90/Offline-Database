06ea224e57d5c5dd775a6539cfd7c4f9e57230b6
RVerma90 <irahulverma90@gmail.com>
2018-08-13 11:59:45 +0100

    added ref push functionality

--- OfflineDatabase.js
+++ OfflineDatabase.js
@@ -3,8 +3,55 @@ const storage = require('node-persist');
 const uuid = require('uuid/v4');
 
 module.exports = class OfflineDatabase {
-  constructor() {
+    constructor(path) {
+        this.dir = path;
+        this.path = path;
+        this.init();
+    }
 
-  }
+    // node-persist default set up
+    async init(dir) {
+        await storage.init({
+            dir: `relative/path/to/persist/${dir}`,
+            stringify: JSON.stringify,
+            parse: JSON.parse,
+            encoding: 'utf8',
+            logging: false, // can also be custom logging function
+            ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS
+            expiredInterval: 2 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
+            // in some cases, you (or some other service) might add non-valid storage files to your
+            // storage dir, i.e. Google Drive, make this true if you'd like to ignore these files and not throw an error
+            forgiveParseErrors: false,
+        });
+    }
 
+    // Wrapping each functions with async, and await them.
+    // This allows us to keep the functions asynchronous.
+    async ref(path) {
+        if (path) {
+            const paths = path.split('/');
+            this.dir = paths[0];
+            this.path = path;
+            await this.init(this.dir);
+        }
+
+        return this;
+    }
+
+    async push(obj) {
+        const id = uuid();
+        try {
+            await this.init(this.dir);
+            await storage.setItem(id, obj);
+        } catch (error) {
+            console.log(error);
+        }
+        return id;
+    }
+
+    async once(cb) {
+        await this.init(this.dir);
+        const res = await storage.getItem(key);
+        cb(res);
+    }  
 };
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const OfflineDatabase = require('./OfflineDatabase');
+
+const db = new OfflineDatabase('test1');
+
+async function main() {
+    let testUser = {
+        name: 'Alice',
+        email: 'alice@email.com',
+    };
+
+    //   
+    const users = await db.ref('users');
+    const newUUID = await users.push(testUser);
+    console.log(testUser.name, 'is a new user with UUID:',newUUID);
+}
+
+main();
